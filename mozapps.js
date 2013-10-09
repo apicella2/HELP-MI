e68f8eaa3a34e7f81d743b121fab2c8e3d664804
apicella2 <apicella2@gmail.com>
2013-10-09 22:35:22 +0200

    First commit

--- mozapps.js
+++ mozapps.js
@@ -15,10 +15,30 @@ var mozApps = (function() {
         self = selfReq.result;
     };
 
+	function installed(success, error) {
+		var r = navigator.mozApps.checkInstalled(manifestURL);
+		r.onerror = function(e) {
+			alert("Error calling checkInstalled: " + r.error.name);
+		};
+		r.onsuccess = function(e) {
+			b = document.getElementById('installa');
+			if (r.result) {
+				//alert("App is installed!");
+				b.style = "display:none;";
+			}
+			else {
+				b.style = "display:block;position:absolute;top:0px;left:0px;z-index:100;";
+				//install();
+			}
+		}
+		return r;
+	}  
     function isRunning() {
+		//alert('isrunning');
         return !!self;
     }
     function install(success, error) {
+		//alert('install');
         var r = navigator.mozApps.install(manifestURL);
         r.onsuccess = success;
         r.onerror = error;
@@ -28,11 +48,13 @@ var mozApps = (function() {
         return r;
     }
     function uninstall() {
+		//alert('unistall');
         if (self)
             return self.uninstall();
     }
 
     return {
+		installed: installed,
         isRunning: isRunning,
         install: install,
         uninstall: uninstall,
